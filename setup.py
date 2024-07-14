5f1a9b51477e8e01af1f98f00aa6d71ffe4ea010
Mikhaylov Nikita <nixncom@gmail.com>
2024-07-14 20:32:59 +0300

    Update README.md, clean apgwrap.py, update __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='asyncpg_wrapper',
-    version='0.1.0',
+    version='0.1.1',
     description='AsyncPG Easy-To-Use wrapper',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
